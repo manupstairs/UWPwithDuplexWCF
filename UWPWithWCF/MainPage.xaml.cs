5fbe6d13f387360f466337a93e5925d56b98f6da
xingzheng <xingzheng@hotmail.com>
2020-02-24 16:47:23 +0800

    add package project

--- UWPWithWCF/MainPage.xaml.cs
+++ UWPWithWCF/MainPage.xaml.cs
@@ -5,8 +5,10 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.ServiceModel;
 using UWPWithWCF.BrightnessService;
+using Windows.ApplicationModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Foundation.Metadata;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -24,15 +26,11 @@ namespace UWPWithWCF
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        private BrightnessNotificationServiceClient brightnessClient = new BrightnessNotificationServiceClient();
+        private BrightnessNotificationServiceClient brightnessClient { get; set; }
 
         public MainPage()
         {
             this.InitializeComponent();
-
-            var binding = new NetTcpBinding(SecurityMode.None);
-            var address = new EndpointAddress("net.tcp://localhost:8001/BrightnessNotificationService");
-            brightnessClient = new BrightnessNotificationServiceClient(binding, address);
         }
 
         private async void BrightnessClient_NotifyBrightnessReceived(object sender, NotifyBrightnessReceivedEventArgs e)
@@ -46,6 +44,15 @@ namespace UWPWithWCF
 
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
+            if (ApiInformation.IsApiContractPresent("Windows.ApplicationModel.FullTrustAppContract", 1, 0))
+            {
+                await FullTrustProcessLauncher.LaunchFullTrustProcessForCurrentAppAsync();
+            }
+
+            var binding = new NetTcpBinding(SecurityMode.None);
+            var address = new EndpointAddress("net.tcp://localhost:8001/BrightnessNotificationService");
+            brightnessClient = new BrightnessNotificationServiceClient(binding, address);
+
             await brightnessClient.SubscribeBrightnessNotificationAsync();
             brightnessClient.NotifyBrightnessReceived += BrightnessClient_NotifyBrightnessReceived;
         }
